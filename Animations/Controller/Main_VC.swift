39bc05c980eff88145a8ea5c8a3f181201d6bef4
Yusif Aliyev <yusif.projects@gmail.com>
2018-03-08 06:47:29 +0400

    Basic Mechanics Are Working.

--- Animations/Controller/Main_VC.swift
+++ Animations/Controller/Main_VC.swift
@@ -4,7 +4,68 @@
 import UIKit
 
 class Main_VC: UIViewController {
+    
+    @IBOutlet weak var box: UIView!
+    
+    var is_on_the_left: Bool!
+    var is_on_the_right: Bool!
+    var is_above: Bool!
+    var is_below: Bool!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        is_on_the_left = true
+        is_on_the_right = false
+        is_above = true
+        is_below = false
+    }
+    
+    func go_right(box: UIView){
+        if self.is_on_the_right == false {
+            box.center.x = box.center.x - 50 - box.frame.width*2 + self.view.frame.width
+            self.is_on_the_right = true
+            self.is_on_the_left = false
+        }
+    }
+    
+    func go_left(box: UIView){
+        if self.is_on_the_left == false {
+            box.center.x = box.center.x + 100 + box.frame.width - self.view.frame.width
+            self.is_on_the_left = true
+            self.is_on_the_right = false
+        }
+    }
+    
+    func go_up (box: UIView){
+        if self.is_above == false {
+            box.center.y = box.center.y + 100 + box.frame.height - self.view.frame.height
+            self.is_above = true
+            self.is_below = false
+        }
+    }
+    
+    func go_down(box: UIView){
+        if self.is_below == false {
+            box.center.y = box.center.y - 50 - box.frame.height*2 + self.view.frame.height
+            self.is_below = true
+            self.is_above = false
+        }
+    }
+    
+    @IBAction func go_right_pressed(_ sender: Any) {
+        go_right(box: box)
+    }
+    
+    @IBAction func go_left_pressed(_ sender: Any) {
+        go_left(box: box)
+    }
+    
+    @IBAction func go_up_pressed(_ sender: Any) {
+        go_up(box: box)
+    }
+    
+    @IBAction func go_down_pressed(_ sender: Any) {
+        go_down(box: box)
     }
 }
