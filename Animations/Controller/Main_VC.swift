1fcc53bb9bf7de976c2d13c7ddb70450cef40a7c
Yusif Aliyev <yusif.projects@gmail.com>
2018-03-08 07:13:54 +0400

    Added a function that disables and enables buttons.

--- Animations/Controller/Main_VC.swift
+++ Animations/Controller/Main_VC.swift
@@ -7,6 +7,11 @@ class Main_VC: UIViewController {
     
     @IBOutlet weak var box: UIView!
     
+    @IBOutlet weak var right_button: UIButton!
+    @IBOutlet weak var down_button: UIButton!
+    @IBOutlet weak var left_button: UIButton!
+    @IBOutlet weak var up_button: UIButton!
+    
     var duration: Double!
     
     var is_on_the_left: Bool!
@@ -19,41 +24,78 @@ class Main_VC: UIViewController {
         
         duration = 0.5
         
-        is_on_the_left = true
-        is_on_the_right = false
-        is_above = true
-        is_below = false
+        up_button.setTitleColor(UIColor.green, for: .normal)
+        down_button.setTitleColor(UIColor.green, for: .normal)
+        left_button.setTitleColor(UIColor.green, for: .normal)
+        right_button.setTitleColor(UIColor.green, for: .normal)
+        
+        up_button.setTitleColor(UIColor.red, for: .disabled)
+        down_button.setTitleColor(UIColor.red, for: .disabled)
+        left_button.setTitleColor(UIColor.red, for: .disabled)
+        right_button.setTitleColor(UIColor.red, for: .disabled)
+        
+        check_position(input: "DEFAULT")
+    }
+    
+    func check_position(input: String){
+        switch input {
+        case "GO-UP":
+            self.is_above = true
+            self.is_below = false
+            self.up_button.isEnabled = false
+            self.down_button.isEnabled = true
+        case "GO-DOWN":
+            self.is_below = true
+            self.is_above = false
+            self.down_button.isEnabled = false
+            self.up_button.isEnabled = true
+        case "GO-LEFT":
+            self.is_on_the_left = true
+            self.is_on_the_right = false
+            self.left_button.isEnabled = false
+            self.right_button.isEnabled = true
+        case "GO-RIGHT":
+            self.is_on_the_right = true
+            self.is_on_the_left = false
+            self.left_button.isEnabled = true
+            self.right_button.isEnabled = false
+        default:
+            self.is_on_the_left = true
+            self.is_on_the_right = false
+            self.is_above = true
+            self.is_below = false
+            self.left_button.isEnabled = false
+            self.right_button.isEnabled = true
+            self.up_button.isEnabled = false
+            self.down_button.isEnabled = true
+        }
     }
     
     func go_right(box: UIView){
         if self.is_on_the_right == false {
             box.center.x = box.center.x - 50 - box.frame.width*2 + self.view.frame.width
-            self.is_on_the_right = true
-            self.is_on_the_left = false
+            check_position(input: "GO-RIGHT")
         }
     }
     
     func go_left(box: UIView){
         if self.is_on_the_left == false {
             box.center.x = box.center.x + 100 + box.frame.width - self.view.frame.width
-            self.is_on_the_left = true
-            self.is_on_the_right = false
+            check_position(input: "GO-LEFT")
         }
     }
     
     func go_up (box: UIView){
         if self.is_above == false {
             box.center.y = box.center.y + 100 + box.frame.height - self.view.frame.height
-            self.is_above = true
-            self.is_below = false
+            check_position(input: "GO-UP")
         }
     }
     
     func go_down(box: UIView){
         if self.is_below == false {
             box.center.y = box.center.y - 50 - box.frame.height*2 + self.view.frame.height
-            self.is_below = true
-            self.is_above = false
+            check_position(input: "GO-DOWN")
         }
     }
     
